adb36a52f3ce4310eb6a53b075ad9c46adce6fe2
beauxq <beauxq@users.noreply.github.com>
2016-10-04 20:47:35 -0500

    first page layout

--- Lab6/Source/Lab6/app/src/main/java/doughoskisson/cs5551/lab6/SignUp.java
+++ Lab6/Source/Lab6/app/src/main/java/doughoskisson/cs5551/lab6/SignUp.java
@@ -2,6 +2,7 @@ package doughoskisson.cs5551.lab6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void proceedButtonClick(View view) {
+    }
+
+    public void imageButtonClick(View view) {
     }
 }
