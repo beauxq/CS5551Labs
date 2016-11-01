cffd2bfbe9c006cfb681ea7e3992b94045528779
beauxq <beauxq@users.noreply.github.com>
2016-11-01 16:30:32 -0500

    add stuff from tutorial 10

---
+++ Lab10/Source/lab10.js
@@ -0,0 +1,102 @@
+var PORT = 8081;
+var SUCCESS = 200;
+var ENDPOINT = "/data";
+var DATABASE_URL = "mongodb://beauxq:beauxq@ds051873.mlab.com:51873/tut8test";
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require("body-parser");
+var express = require('express');
+var request = require('request');
+
+var application = express();
+
+application.use(cors());
+application.use(bodyParser.json());
+application.use(bodyParser.urlencoded({ extended: true }));
+
+application.post(ENDPOINT, function (req, res) {
+    MongoClient.connect(DATABASE_URL, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        insertDocument(db, req.body, function() {
+            res.write("Successfully inserted");
+            res.end();
+        });
+    });
+});
+
+application.get(ENDPOINT, function (req, res) {
+    var result={
+        "thing": req.query.thing,
+        "type": "",
+        "shortUrl": ""
+    };
+
+    if (! result.thing) {
+        return console.log('Error: parameter "thing" required');
+    }
+
+    request(API1 + result.thing, function (error, response, body) {
+        if(error){
+            return console.log('API1 error:', error);
+        }
+
+        if(response.statusCode !== SUCCESS){
+            return console.log('API1 invalid status code returned:', response.statusCode);
+        }
+
+        console.log(body);
+        body = JSON.parse(body);
+        var description = body.itemListElement[0].result.description;
+        var url = body.itemListElement[0].result.detailedDescription.url;
+        result.type = description;
+
+        request.post({
+            url: API2,
+            json: { longUrl: url }
+        }, function(error, response, body){
+            if(error){
+                return console.log('API2 error:', error);
+            }
+
+            if(response.statusCode !== SUCCESS){
+                return console.log('API2 invalid status code returned:', response.statusCode);
+            }
+
+            console.log(body);
+            // body = JSON.parse(body);
+            result.shortUrl = body.id;
+
+            res.contentType('application/json');
+            res.write(JSON.stringify(result));
+            console.log(result);
+            res.end();
+        });
+    });
+});
+
+var insertDocument = function(db, data, callback) {
+    db.collection('demoase').insertOne( data, function(err, result) {
+        if(err)
+        {
+            res.write("Registration Failed, Error While Registering");
+            res.end();
+        }
+        console.log("Inserted a document into the restaurants collection.");
+        callback();
+    });
+};
+
+// static files served
+application.use(express.static("public"));
+
+var server = application.listen(PORT, function () {
+    var port = server.address().port;
+
+    console.log("server listening at http://localhost:%s", port);
+    console.log("serving API at http://localhost:%s%s", port, ENDPOINT)
+});
--- Lab10/Source/public/index.js
+++ Lab10/Source/public/index.js
@@ -2,46 +2,81 @@
  * JavaScript for index.html
  */
 
-angular.module("lab10", [])
+var application = angular.module("lab10", []);
 
-    .directive('enterPress', function () {
-        return function (scope, element, attrs) {
-            element.bind("keydown keypress", function (event) {
-                if(event.which === 13) {
-                    scope.$apply(function (){
-                        scope.$eval(attrs.enterPress);
-                    });
+application.directive('enterPress', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.enterPress);
+                });
 
-                    event.preventDefault();
-                }
-            });
-        };
-    })
+                event.preventDefault();
+            }
+        });
+    };
+});
 
-    .controller("lab10Controller", function($scope, $http) {
-        $scope.state = "start";
+application.run(function ($http) {
+    // Sends this header with any AJAX request
+    $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    // Send this header only in post requests. Specifies you are sending a JSON object
+    $http.defaults.headers.post['dataType'] = 'json'
+});
 
-        $scope.startLoginButtonClick = function() {
-            $scope.state = "login";
-        };
-        $scope.startRegisterButtonClick = function() {
-            $scope.state = "reg";
-        };
+application.controller("lab10Controller", function($scope, $http) {
+    $scope.state = "start";
 
-        $scope.loginLoginButtonClick = function() {
-            $scope.state = "home";
-        };
+    $scope.startLoginButtonClick = function() {
+        $scope.state = "login";
+    };
+    $scope.startRegisterButtonClick = function() {
+        $scope.state = "reg";
+    };
 
-        $scope.registerRegisterButtonClick = function() {
-            $scope.loginUsername = $scope.registerUsername;
-            $scope.state = "login";
-        };
+    $scope.loginLoginButtonClick = function() {
+        $scope.state = "home";
+    };
 
-        $scope.getInformationButtonClick = function() {
-            console.log("button clicked");
-            $http.get("thingInfo?thing=" + $scope.thing).then(function(response) {
-                $scope.description = response.data.type;
-                $scope.shortUrl = response.data.shortUrl;
-            });
-        };
-    });
+    $scope.registerRegisterButtonClick = function() {
+        /*
+
+         console.log($scope.formData.lname);
+         console.log($scope.formData.fname);
+         console.log($scope.formData.email);
+         console.log($scope.formData.password);
+         console.log($scope.formData.cpassword);
+         var dataParams = {
+         'fname' : $scope.fname,
+         'lname' : $scope.lname,
+         'email' : $scope.email,
+         'pw' : $scope.pw
+         };
+         var config = {
+         headers : {
+         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+         }
+         }
+         var req = $http.post('http://127.0.0.1:8081/register',$scope.formData);
+         req.success(function(data, status, headers, config) {
+         $scope.message = data;
+         console.log(data);
+         });
+         req.error(function(data, status, headers, config) {
+         alert( "failure message: " + JSON.stringify({data: data}));
+         });
+         */
+
+        $scope.loginUsername = $scope.registerUsername;
+        $scope.state = "login";
+    };
+
+    $scope.getInformationButtonClick = function() {
+        console.log("button clicked");
+        $http.get("thingInfo?thing=" + $scope.thing).then(function(response) {
+            $scope.description = response.data.type;
+            $scope.shortUrl = response.data.shortUrl;
+        });
+    };
+});
